53054bc9a4c30837ec28b571a8682456045acffe
Facundo <facuchaer@gmail.com>
2018-10-08 01:35:11 -0300

    dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title','UON UTN - FRTDF')
+
+
+@section('navbar')
+  <a href="">
+    <picture>
+      <source srcset="{{URL::asset('/img/logo/s5_logo.png')}}">
+      <img src="{{URL::asset('/img/logo/s5_logo.png')}}" alt="Logo Utn">
+    </picture>
+  </a>
+  <span> UON - Gestión administrativa </span>
+@endsection
+
+@section('content')
+  <a href="">
+    
+  </a>
+@endsection
