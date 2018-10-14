316a08d370f7c677d2a6c555a0d08d084757cd97
Facundo <facuchaer@gmail.com>
2018-10-14 04:10:58 -0300

    main page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,52 +12,15 @@
 
     </head>
     <body>
-        {{-- head section --}}
-        <head>
-            @section('header')
-            @show
-        </head>
-        {{-- nav section --}}
-        <nav class="navbar navbar-expand-md navbar-dark">
-            @section('navbar')
-            @show
-        </nav>
-        {{-- content section --}}
-        <main class="container-fluid" id="main">
-            <div class="row">
-                {{-- left bar --}}
-                <div class="col-md-3 col-lg-2" id="sidebar" role="navigation">
-                    @section('left-bar')
-                    @show
-                </div>
-                {{-- main content --}}
-                <div class="col main pt-5 mt-3">
-                    @section('content')
-                    section content
-                </div>
-                {{-- right bar section --}}
-                <div class="col-md-3 col-lg-2">
-                    @section('right-bar')
-                    @show
-                </div>
-            </div>
-        </main>
-        {{-- footer section --}}
-        <footer class="container-fluid">
-            @section('footer')
-            section footer
-        </footer>
-
-        <div>
-            @section('messages')
-        </div>
-
-        @section('scripts')
-        <!--scripts loaded here-->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <div id="app"></div>
+        <!--scripts loaded here
+        
         <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
         <script src="{{URL::asset('/js/theme.js')}}"></script>
+        <script src="{{URL::asset('/build/UOM_main.js')}}"></script>
+        
 
     </body>
 </html>
