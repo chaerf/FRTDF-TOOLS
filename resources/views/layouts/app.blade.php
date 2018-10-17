45f0ad01f8d35057a726e176a4e6b36def591efe
Facundo <facuchaer@gmail.com>
2018-10-17 17:43:47 -0300

    layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,12 @@
         <meta name="author" content="Facundo Chaer - facuchaer@gmail.com">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-
+        <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Inconsolata:300|Material+Icons' rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="app"></div>
         <!--scripts loaded here-->
-        
         <script src="{{URL::asset('/build/UOM_main.js')}}"></script>
-        
-
     </body>
 </html>
